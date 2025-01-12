9a114b06b5c98834ecb77f7f49b11436d26c1542
Vincenzo Moussa <vincenzomoussa@gmail.com>
2025-01-12 17:35:49 +0100

    Animazione sistemata

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,5 @@
+/* Evento per aggiungere una classa alla navbar allo scroll. */
+
 window.addEventListener("scroll", function () {
   var navbar = document.getElementById("barra");
   var getStart = document.getElementById("getStart");
@@ -9,3 +11,20 @@ window.addEventListener("scroll", function () {
     getStart.classList.remove("getStartedChange");
   }
 });
+
+/* Funzione per illuminare le "emme" nell'html. */
+
+let emme = document.querySelector("svg");
+
+let gruppoEmme = [...emme.querySelectorAll('g[opacity="1"]')];
+
+function randomOpacity() {
+  let index = Math.floor(Math.random() * gruppoEmme.length + 1);
+  let gruppo = gruppoEmme[index];
+
+  gruppo.setAttribute("opacity", 0);
+  setTimeout(() => {
+    gruppo.setAttribute("opacity", 1);
+  }, 1500);
+}
+setInterval(randomOpacity, 100);
